521a1a6601bdd5552c5feb5f1f4ba43d7bd78ada
Phornsri <ponsri_Molae074@yahoo.com>
2020-11-01 11:39:55 -0500

    Bat mobile

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,5 +1,9 @@
 public class Batman {
     public static void main(String[] args) {
+        System.out.println("I will catch you!");
+        // OK come on!
+        //Batman has cool Bat mobile
+        // yes, fine
 
     }
 
